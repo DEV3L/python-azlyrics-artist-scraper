f86a8845dd68e43ca5b525a4513e1320255aa36f
DEV3L <jus.beall@gmail.com>
2016-11-07 23:33:46 -0500

    Refactor to start using instance, not class methods

--- azlyrics_scrape.py
+++ azlyrics_scrape.py
@@ -11,17 +11,18 @@ import bs4
 
 from logging_wrapper import log_message, log_exception
 
-AZLYRICS_URL = 'http://www.azlyrics.com/t/taylorswift.html'
+AZLYRICS_ARTIST_URL = 'http://www.azlyrics.com/t/taylorswift.html'
 CACHE_DIR = 'html/'
 
 
 class AZLyricsScrape:
+    def __init__(self, *, artist_url=AZLYRICS_ARTIST_URL):
+        self.artist_url=artist_url
+        self.artist=artist_url[artist_url.rfind('/')+1:artist_url.rfind('.')]
+        pass
+
     @classmethod
-    def get_song_info(cls, url):
-        """
-        Grabs lyrics from URL and return plain text lyrics.
-        Lyrics are located at /html/body/div[3]/div/div[2]/div[6]
-        """
+    def get_song_info(cls, url, artist):
         if cls.load_local(url):
             soup = bs4.BeautifulSoup(cls.load_local(url), 'html.parser')
         else:
@@ -44,8 +45,8 @@ class AZLyricsScrape:
             return None
 
         album_str = album.replace(' ', '_').replace(':', '') if album else None
-        title_str = title.replace(' ', '_').replace(':', '').replace("'", '').replace('/', '').replace(',', '').replace(
-                '?', '')
+        title_str = title.replace(' ', '_').replace(':', '').replace("'", '') \
+            .replace('/', '').replace(',', '').replace('?', '')
 
         entry_info = {
             'lyrics': lyrics,
@@ -55,29 +56,27 @@ class AZLyricsScrape:
         }
 
         if album is not None:
-            output_file_dir = 'data/%s_%s/' % (album_str, year)
+            output_file_dir = 'data/%s/%s_%s/' % (artist, year, album_str)
         else:
-            output_file_dir = 'data/noalbum/'
+            output_file_dir = 'data/%s/no_album/' % (artist)
 
         if not isdir(output_file_dir):
             makedirs(output_file_dir)
 
         output_filename = join(output_file_dir, '%s.json' % title_str)
-        f = open(output_filename, 'w')
-        json.dump(str(entry_info), f, sort_keys=1, indent=2)
-        f.close()
+        write_file = open(output_filename, 'w')
+        json.dump(str(entry_info), write_file, sort_keys=1, indent=2)
+        write_file.close()
 
         log_message('Parsed %s' % title)
         return entry_info
 
     @classmethod
     def get_lyrics(cls, soup):
-        """
-        Method for extracting lyrics from BeautifulSoup. Relies on the fact that
-        lyrics are very long instead of relying on consistent formatting from AZ Lyrics.
-        """
         all_div = soup.findAll('div')
-        all_text = [i.text for i in all_div if i.attrs.get('class') and 'main-page' in i.attrs.get('class')][0].strip()
+        all_text = [i.text for i in all_div
+                    if i.attrs.get('class') and 'main-page'
+                    in i.attrs.get('class')][0].strip()
         return_text = ''
 
         for line in [i for i in all_text.split('\n') if i.strip()][5:]:
@@ -103,7 +102,7 @@ class AZLyricsScrape:
     @classmethod
     def url_to_filename(cls, url):
         """
-        Make a URL into a file name, using SHA1 hashes.
+        SHA1 hash URL as a file name
         """
         hash_file = sha1(str.encode(url)).hexdigest() + '.html'
         return join(CACHE_DIR, hash_file)
@@ -127,24 +126,27 @@ class AZLyricsScrape:
             log_message('Loaded locally: %s' % url)
             return f.read()
 
-    @classmethod
-    def scrape_artist_page(cls, home_url, *, output_dir=''):
-        soup = cls.get_soup_from_url(home_url)
-        song_lst = soup.find(id='listAlbum').findAll(target='_blank')
+    def scrape_artist_page(self):
+        soup = self.get_soup_from_url(self.artist_url)
+        list_of_songs = soup.find(id='listAlbum').findAll(target='_blank')
 
         had_previous = False
-        filename = join(output_dir, 'az_lyrics.json')
+        filename = '%s_%s.%s' % ('az_lyrics', self.artist, '.json')
+
         with open(filename, 'w') as f:
-            for song in song_lst:
+            for song in list_of_songs:
                 f.write(',' if had_previous else '[')
                 had_previous = True
-                song_url = urljoin(home_url, song.get('href'))
-                song_info = cls.get_song_info(song_url)
+                song_url = urljoin(self.artist_url, song.get('href'))
+                song_info = self.get_song_info(song_url, self.artist)
                 log_message(song_info['lyrics'])
                 json.dump(song_info, f)
             f.write(']')
+
         log_message('Done writing files')
 
 
 if __name__ == '__main__':
-    AZLyricsScrape.scrape_artist_page(AZLYRICS_URL)
+    azlyrics_scrape = AZLyricsScrape(artist_url=AZLYRICS_ARTIST_URL)
+    azlyrics_scrape.scrape_artist_page()
+    # AZLyricsScrape.scrape_artist_page(AZLYRICS_ARTIST_URL)
