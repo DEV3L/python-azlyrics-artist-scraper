b44ddf19b9e5d716786389c627d2fc8bbcabd6b9
DEV3L <jus.beall@gmail.com>
2016-11-13 18:54:26 -0500

    Update lyrics_corpus_writer file location

--- app/lyrics_corpus_writer.py
+++ app/lyrics_corpus_writer.py
@@ -4,14 +4,18 @@ from os.path import join
 
 
 class LyricsCorpusWriter:
+    corpus_file_extension = '.txt'
+
     def __init__(self, artist, data_dir):
         self.artist = artist
         self.data_dir = data_dir
 
     def write(self):
-        with open(join(self.data_dir, self.artist) + self.artist + '.txt', 'w') as corpus_file:
+        with open(join(self.data_dir, self.artist, self.artist) + self.corpus_file_extension, 'w') as corpus_file:
             for cur, _, files in walk(join(self.data_dir, self.artist)):
                 for file in files:
+                    if file.endswith(self.corpus_file_extension):
+                        continue
                     with open(join(cur, file), 'r') as json_data:
                         corpus_file.write(self._remove_non_ascii(json.load(json_data).get('lyrics')))
                         corpus_file.write('\n')
